9f8b04e03fb763a2865bd21ba3b49f738fa17c3d
kyosora <coasta98381@gmail.com>
2023-03-01 11:44:57 +0800

    add Nlog

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 class Program
 {
+    private static void CreateLogger()
+    {
+        var config = new LoggingConfiguration();
+        var fileTarget = new FileTarget
+        {
+            FileName = "${basedir}/logs/${shortdate}.log",
+            Layout = "${date:format=yyyy-MM-dd HH\\:mm\\:ss} [${uppercase:${level}}] ${message}",
+        };
+        config.AddRule(LogLevel.Trace, LogLevel.Fatal, fileTarget);
+        LogManager.Configuration = config;
+    }
     static void Main(string[] args)
     {
+        // 1.不使用外部設定檔
+        // CreateLogger();
+        // 2.使用外部設定檔
+        LogManager.Configuration = new XmlLoggingConfiguration("Configs/NLog.config");
+        Logger logger = LogManager.GetCurrentClassLogger();
+
+        logger.Trace("Start");
+
         // 檢查是否提供了根目錄路徑參數
         if (args.Length == 0)
         {
@@ -12,7 +34,7 @@ class Program
             string userInput = Console.ReadLine().Trim();
             if (string.IsNullOrEmpty(userInput))
             {
-                Console.WriteLine("根目錄路徑不能為空。");
+                logger.Warn("根目錄路徑不能為空");
                 return;
             }
             args = new[] { userInput };
@@ -22,7 +44,7 @@ class Program
 
         if (!Directory.Exists(rootFolderPath))
         {
-            Console.WriteLine($"根目錄 '{rootFolderPath}' 不存在。");
+            logger.Warn($"根目錄 '{rootFolderPath}' 不存在。");
             return;
         }
 
@@ -34,7 +56,6 @@ class Program
         {
             // 取得資料夾名稱
             var folderName = new DirectoryInfo(folderPath).Name;
-
             // 檢查是否有與其同名的父資料夾
             var parentFolder = new DirectoryInfo(folderPath).Parent;
             while (parentFolder != null && parentFolder.Name == folderName)
@@ -48,6 +69,7 @@ class Program
                     if (!Directory.Exists(newFileDirectory))
                         Directory.CreateDirectory(newFileDirectory);
                     File.Move(file, newFilePath);
+                    logger.Debug("Move " + file + " -> " + newFilePath);
                 }
                 var subFolders = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories);
                 foreach (var subFolder in subFolders)
@@ -57,6 +79,7 @@ class Program
                     if (!Directory.Exists(newFolderPath))
                     {
                         Directory.Move(subFolder, newFolderPath);
+                        logger.Debug("Move " + subFolder + " -> " + newFolderPath);
                     }
                 }
 
@@ -65,7 +88,9 @@ class Program
                 {
                     if (Directory.GetFiles(folderPath).Length == 0)
                     {
+
                         Directory.Delete(folderPath, true);
+                        logger.Debug("delete " + folderPath);
                     }
                 }
 
@@ -73,7 +98,6 @@ class Program
                 parentFolder = parentFolder.Parent;
             }
         }
-
-        Console.WriteLine("完成");
+        logger.Trace("End");
     }
 }
